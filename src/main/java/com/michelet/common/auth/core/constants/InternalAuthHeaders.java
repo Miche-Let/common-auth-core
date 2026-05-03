aeb8e783d6ab996f4ed47d0a685d666e7b4911c9
Sehi55 <sehi1028@naver.com>
2026-05-04 00:09:43 +0900

    feat: add header for internal token

---
+++ src/main/java/com/michelet/common/auth/core/constants/InternalAuthHeaders.java
@@ -0,0 +1,7 @@
+package com.michelet.common.auth.core.constants;
+
+public final class InternalAuthHeaders {
+    public static final String INTERNAL_TOKEN = "X-Internal-Token";
+
+    private InternalAuthHeaders() {
+    }}
